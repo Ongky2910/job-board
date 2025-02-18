2ef45a4014592cf3d893b404dd8de28c66bfc1b4
Ongky2910 <ongkypermana21@gmail.com>
2025-02-18 19:38:27 +0800

    Delete Documents/react-harisenin directory

--- Documents/react-harisenin/my-app/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer className="bg-dark text-white text-center p-3">
-    <p>&copy; 2024 My App</p>
-  </footer>
-);
-
-export default Footer;
