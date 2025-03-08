0d8a85b6bf859cde2d1ed8bb2b6c7fd44b65cca2
ongkypermana21@gmail.com <ongkypermana21@gmail.com>
2025-03-08 09:42:54 +0800

    Remove node_modules from Git tracking

--- models/User.js
+++ models/User.js
@@ -1,20 +1,34 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-  displayName: { type: String, required: true },
-  email: {
-    type: String,
-    required: false,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    displayName: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],
+    },
+    password: { type: String, required: true },
+    savedJobs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Job" }],
+    appliedJobs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Job" }],
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  savedJobs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Job" }], 
-  appliedJobs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Job" }], 
+  { timestamps: true }
+);
+
+// **Middleware untuk Hash Password Sebelum Disimpan**
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
-
 module.exports = User;
